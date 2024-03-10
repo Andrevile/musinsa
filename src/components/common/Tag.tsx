aebd789f5a4d78b878daca87ebe42a676b86bbb5
abg3000 <abg3000@naver.com>
2024-03-10 16:59:06 +0900

    feat: 태그 컴포넌트

---
+++ src/components/common/Tag.tsx
@@ -0,0 +1,46 @@
+import { css } from '@emotion/react';
+import { HTMLAttributes } from 'react';
+
+import { CloseIcon } from './icons/CloseIcon';
+
+interface Props extends Omit<HTMLAttributes<HTMLSpanElement>, 'onClick'> {
+  item: { id: string; text: string };
+  closable?: boolean;
+  onClose?: (id: string) => void;
+}
+
+export function Tag({ item, closable = true, onClose, ...rest }: Props) {
+  return (
+    <span
+      css={css`
+        color: #ffffff;
+        display: inline-flex;
+        align-items: center;
+        justify-content: center;
+        flex-shrink: 0;
+        height: 26px;
+        border-radius: 4px;
+        background: #0078ff;
+        font-size: 12px;
+        font-weight: 400;
+        line-height: 1.5;
+        padding: 4px 8px;
+      `}
+      {...rest}
+    >
+      <span>{item.text}</span>
+      {closable && (
+        <span
+          onClick={() => onClose?.(item.id)}
+          css={css`
+            position: relative;
+            top: 2px;
+            margin-left: 4px;
+          `}
+        >
+          <CloseIcon />
+        </span>
+      )}
+    </span>
+  );
+}
