beb5691002e79a4871cd311884acf09268275e1d
abg3000 <abg3000@naver.com>
2024-03-14 22:59:15 +0900

    fix: 검색어 입력 \ 짝수번 입력 에러 대응

--- src/utils/getKeywordRegex.ts
+++ src/utils/getKeywordRegex.ts
@@ -1,6 +1,11 @@
 export const getKeywordRegex = (keyword: string) => {
-  const escapedKeyword = keyword.replace(/[^ㄱ-ㅎ|ㅏ-ㅣ|가-햫|a-z|A-Z|0-9|\s]+/g, '\\$&'); // 이스케이프 처리
-  const regex = new RegExp(`(${escapedKeyword})`, 'gi');
-
-  return regex;
+  try {
+    const escapedDash = keyword.replace(/\\/g, '\\');
+    const escapedKeyword = escapedDash.replace(/[^ㄱ-ㅎ|ㅏ-ㅣ|가-햫|a-z|A-Z|0-9|\s]/g, '\\$&');
+    const regex = new RegExp(`(${escapedKeyword})`, 'gi');
+    return regex;
+  } catch (error) {
+    console.log(error);
+    throw error;
+  }
 };
